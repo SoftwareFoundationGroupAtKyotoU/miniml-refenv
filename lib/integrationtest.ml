f0d78438f40a715b6f34169413f05e17a5e1576d
Yuito Murase <yuito@acupof.coffee>
2024-06-10 10:26:50 +0900

    guarded_div example is working!

--- lib/integrationtest.ml
+++ lib/integrationtest.ml
@@ -117,4 +117,59 @@ let%test_unit "big test cases" =
     ~expect:(Option.Some(BaseInt));
   [%test_result: Evaluator.Value.t]
     (subject |> Evaluator.eval_v)
-    ~expect:(Evaluator.Value.Int(4096));
+    ~expect:(Evaluator.Value.Int(4096))
+
+  let%test_unit "big test cases" =
+    let subject = Cui.read_term
+        {|
+           let assert_locus[g1:>!](
+             f: ref (<int->int@g1> -> <int->int@g1>) -> <int->int@g1>
+           ): <int->int@g1> =
+             let r: ref (<int->int@g1> -> <int->int@g1>) = ref (fun (x: <int->int@g1>) -> x) in
+             let c: <int->int@g1> = f r in
+             let transformer: <int->int@g1> -> <int->int@g1> = !r in
+             transformer c in
+
+           let add_assert[g1:>!]
+               (locus: ref (<int->int@g1> -> <int->int@g1>))
+               (transformer: <int->int@g1> -> <int->int@g1>)
+               : unit =
+             locus := (
+               let oldtr: <int->int@g1> -> <int->int@g1> = !locus in
+               fun (x:<int->int@g1>) -> oldtr (transformer x)) in
+
+           let guarded_div[g1:>!][g2:>g1]
+               (locus: ref (<int->int@g1> -> <int->int@g1>))
+               (x: <int@g2>)
+               (y: <int@g1>)
+               : <int@g2> =
+             let () = add_assert@@g1
+                        locus
+                        (fun (z:<int->int@g1>) -> `{@g1 if 0 < ~y then ~z else (fun(w:int)->w)}) in
+             `{@g2 ~x / ~y} in
+
+          `{@!
+             let rec fib (n:int): int =
+               if n < 2 then 1
+               else fib (n - 1) + fib (n - 2) in
+             fun (y:int@h1) -> ~{
+               assert_locus@@h1 (fun (locus:ref (<int->int@h1> -> <int->int@h1>)) ->
+                 `{@h1 fun (z:int@h3) -> (fib 100) + ~{guarded_div@@h1@@h3 locus `{@h3 z } `{@h1 y }}})}
+           }
+
+     |} in
+  [%test_result: Typ.t option]
+    (subject |> Typechecker.typeinfer Context.empty)
+    ~expect:(Option.Some(Typ.(Code(Cls.init, Func(BaseInt, Func(BaseInt, BaseInt))))));
+  [%test_result: Evaluator.Value.t]
+    (subject |> Evaluator.eval_v)
+    ~expect:(Evaluator.Value.Code(
+        {|
+           `{@!
+             let rec fib (n:int): int =
+               if n < 2 then 1
+               else fib (n - 1) + fib (n - 2) in
+             fun (y:int@h1) -> if 0 < y then (fun (z:int@h3) -> (fib 100) + z / y) else (fun (x:int@h3) -> x)
+            }
+         |} |> Cui.read_term
+      ))
